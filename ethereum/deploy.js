3cdd1eedb71265bebebc7d47ad2f88593f76897b
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2019-12-28 20:37:35 +0530

    init commit

---
+++ ethereum/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  'issue spoon spin crisp street tank unique despair apart like live vivid',
+    'https://rinkeby.infura.io/v3/1ec6fd9491f8465cbc293b9d7367bf5f');
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
